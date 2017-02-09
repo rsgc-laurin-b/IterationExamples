25b1b6bf6ff532ec9c5e87f72a7b17782851fb73
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-09 09:26:22 -0500

    Initial version of tutorial

---
+++ IterationExamples/Iteration Examples.playground/Pages/Example.xcplaygroundpage/Contents.swift
@@ -0,0 +1,37 @@
+//: [Previous](@previous) / [Next](@next)
+//: # Exercise
+//: Say that we want to create an output image like this on a 300x300 canvas:
+//:
+//: ![example](example.png "Timeline")
+//:
+//: At first glance this may seem like a complicated image.
+//:
+//: However, if we break down, or *decompose* the problem, into smaller, more easily completed tasks, it's not so bad.
+//:
+//: Take a look at this storyboard:
+//:
+//:![storyboard](storyboard.png "Storyboard")
+//:
+//: Do you think you could combine:
+//: * a single loop
+//: * a single statement that draws a line
+//:
+//: ... to create this image?  Give it a try!
+//:
+//: ## Required code
+//: The following two statements are required to make the playground run. Please do not remove.
+import Cocoa
+import PlaygroundSupport
+
+//: ## Add your code below
+
+// Create canvas
+let canvas = Canvas(width: 300, height: 300)
+
+// Below this line, try combining a loop and a single statement to generate the goal
+
+/*:
+ ## Template code
+ The code below is necessary to see results in the Assistant Editor at right. Please do not remove.
+ */
+PlaygroundPage.current.liveView = canvas.imageView
---
+++ IterationExamples/Iteration Examples.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -0,0 +1,15 @@
+/*:
+ [Next](@next)
+ # Introduction
+ Programmers don't like to do repetitive work.
+ 
+ That's what computers are for!
+
+ This playground will explain how to use *loops* to run a block of code more than once (or *iterate*).
+ 
+ Most of the pages in this review package present visual output in the Assistant Editor.
+ 
+ If you do not see any output, remember to turn on the Assistant Editor, and then click the menu to switch to the Timeline:
+ 
+ ![timeline](timeline.png "Timeline")
+ */
