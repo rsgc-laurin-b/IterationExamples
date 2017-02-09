e1d7b29a77fd6bc082c9358b27089f848280bac2
Russell Gordon <rgordon@rsgc.on.ca>
2017-02-09 08:51:54 -0500

    Initial Commit

---
+++ IterationExamples/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  IterationExamples
+//
+//  Created by Russell Gordon on 2/9/17.
+//  Copyright © 2017 Russell Gordon. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
